a56546601bbb512ad37323461d64c610dd398bb1
tangguhriyadi <mtangguh97@gmail.com>
2025-07-20 06:42:18 +0700

    fix: fixing failed build

--- src/app/(auth)/verify/page.tsx
+++ src/app/(auth)/verify/page.tsx
@@ -1,5 +1,27 @@
 "use client";
 
+import { useRouter, useSearchParams } from "next/navigation";
+import { useEffect } from "react";
+
 export default function Page() {
+    const router = useRouter();
+    const searchParams = useSearchParams();
+
+    useEffect(() => {
+        const token = searchParams.get("token");
+        const params = new URLSearchParams(searchParams.toString());
+
+        if (!token) {
+            window.location.href = process.env.NEXT_PUBLIC_LANDING_URL ?? "";
+            params.delete("delete");
+        } else {
+            localStorage.setItem(
+                process.env.NEXT_PUBLIC_TOKEN_PROPERTY ?? "",
+                decodeURIComponent(token)
+            );
+            params.delete("delete");
+            router.push("/");
+        }
+    }, [searchParams, router]);
     return <div>...</div>;
 }
