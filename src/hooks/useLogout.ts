a2b95d128213822c860fcb643ea0413bdded531d
tangguhriyadi <mtangguh97@gmail.com>
2025-07-21 10:28:41 +0700

    fix: logout

--- src/hooks/useLogout.ts
+++ src/hooks/useLogout.ts
@@ -3,7 +3,9 @@
 export default function useLogout() {
     const logout = () => {
         localStorage.removeItem(process.env.NEXT_PUBLIC_TOKEN_PROPERTY ?? "");
-        window.location.href = process.env.NEXT_PUBLIC_LANDING_URL ?? "";
+        window.location.href = process.env.NEXT_PUBLIC_LANDING_URL
+            ? process.env.NEXT_PUBLIC_LANDING_URL + "/logout"
+            : "";
     };
 
     return logout;
